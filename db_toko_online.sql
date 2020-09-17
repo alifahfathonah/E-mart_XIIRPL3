c954e49ef0557ea9ef9a822bd8ac882f79081851
rifkyalfariz <rifkyalfariz2003@gmail.com>
2020-09-17 16:44:10 +0700

    Memperbarui database dan menambahkan gambar erd database

--- db_toko_online.sql
+++ db_toko_online.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 10, 2020 at 02:45 AM
--- Server version: 10.4.13-MariaDB
--- PHP Version: 7.2.31
+-- Generation Time: Sep 17, 2020 at 10:19 AM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -29,12 +30,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `tb_akun` (
   `id_akun` int(11) NOT NULL,
-  `nama_depan` varchar(16) NOT NULL,
-  `nama_belakang` varchar(16) NOT NULL,
+  `nama` varchar(64) NOT NULL,
   `email` varchar(64) NOT NULL,
   `username` varchar(16) NOT NULL,
   `katasandi` varchar(128) NOT NULL,
-  `role_id` int(11) NOT NULL,
+  `role_id` int(1) NOT NULL,
   `is_active` int(1) NOT NULL,
   `date_created` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -43,9 +43,9 @@ CREATE TABLE `tb_akun` (
 -- Dumping data for table `tb_akun`
 --
 
-INSERT INTO `tb_akun` (`id_akun`, `nama_depan`, `nama_belakang`, `email`, `username`, `katasandi`, `role_id`, `is_active`, `date_created`) VALUES
-(1, 'Rifky', 'Alfariz', 'rifkyalfariz2003@gmail.com', 'rifkyalfariz', '$2y$10$rTRMcSHRaXPseXOFOKgboecW8LUmQeOX25oXoIhnQE4z8vK4S1ZKS', 2, 1, 1599543823),
-(2, 'yahya', 'abdlah', 'yahyaabdulah14@gmail.com', 'yahya abdulah', '$2y$10$fYHU/9U9QIwwetb2isYZq.5eNBqQIXuClhJJHwhf30hiyjnJ3GTf.', 2, 1, 1599650565);
+INSERT INTO `tb_akun` (`id_akun`, `nama`, `email`, `username`, `katasandi`, `role_id`, `is_active`, `date_created`) VALUES
+(1, 'Rifky', 'rifkyalfariz2003@gmail.com', 'rifkyalfariz', '$2y$10$rTRMcSHRaXPseXOFOKgboecW8LUmQeOX25oXoIhnQE4z8vK4S1ZKS', 1, 1, 1599543823),
+(2, 'yahya', 'yahyaabdulah14@gmail.com', 'yahya abdulah', '$2y$10$fYHU/9U9QIwwetb2isYZq.5eNBqQIXuClhJJHwhf30hiyjnJ3GTf.', 2, 1, 1599650565);
 
 -- --------------------------------------------------------
 
@@ -57,7 +57,8 @@ CREATE TABLE `tb_det_akun` (
   `id_det_akun` int(11) NOT NULL,
   `id_akun` int(11) NOT NULL,
   `alamat` int(11) NOT NULL,
-  `tanggal_lahir` int(11) NOT NULL
+  `tanggal_lahir` int(11) NOT NULL,
+  `img_profil` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -69,8 +70,50 @@ CREATE TABLE `tb_det_akun` (
 CREATE TABLE `tb_det_produk` (
   `id_det_produk` int(11) NOT NULL,
   `id_produk` int(11) NOT NULL,
-  `deskripsi` int(11) NOT NULL
+  `deskripsi` int(11) NOT NULL,
+  `img_produk` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_kategori`
+--
+
+CREATE TABLE `tb_kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `kategori` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_kategori`
+--
+
+INSERT INTO `tb_kategori` (`id_kategori`, `kategori`) VALUES
+(1, 'Elektronik'),
+(2, 'Komputer'),
+(3, 'Kamera'),
+(4, 'Handphone'),
+(5, 'Pakaian Wanita'),
+(6, 'Pakaian Pria'),
+(7, 'Aksesoris'),
+(8, 'Makanan dan Minuman'),
+(9, 'Kesehatan'),
+(10, 'Kecantikan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_keranjang`
+--
+
+CREATE TABLE `tb_keranjang` (
+  `id_keranjang` int(11) NOT NULL,
+  `id_produk` int(11) NOT NULL,
+  `id_akun` int(11) NOT NULL,
+  `harga_total` int(11) NOT NULL,
+  `jumlah_produk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -84,6 +127,21 @@ CREATE TABLE `tb_pembayaran` (
   `total_bayar` int(11) NOT NULL,
   `id_transaksi` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_produk`
+--
+
+CREATE TABLE `tb_produk` (
+  `id_produk` int(11) NOT NULL,
+  `nama_produk` varchar(128) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `id_toko` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -107,15 +165,27 @@ INSERT INTO `tb_role_akun` (`id_role`, `nama_role`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tb_toko`
+--
+
+CREATE TABLE `tb_toko` (
+  `id_toko` int(11) NOT NULL,
+  `id_akun` int(11) NOT NULL,
+  `nama_toko` varchar(64) NOT NULL,
+  `img_toko` varchar(128) NOT NULL,
+  `tgl_dibuat` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tb_transaksi`
 --
 
 CREATE TABLE `tb_transaksi` (
   `id_transaksi` int(11) NOT NULL,
-  `id_barang` int(11) NOT NULL,
-  `id_pembeli` int(11) NOT NULL,
-  `tanggal` date NOT NULL,
-  `keterangan` varchar(100) NOT NULL
+  `id_keranjang` int(11) NOT NULL,
+  `tgl_transaksi` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -126,27 +196,53 @@ CREATE TABLE `tb_transaksi` (
 -- Indexes for table `tb_akun`
 --
 ALTER TABLE `tb_akun`
-  ADD PRIMARY KEY (`id_akun`);
+  ADD PRIMARY KEY (`id_akun`),
+  ADD KEY `role_id` (`role_id`);
 
 --
 -- Indexes for table `tb_det_akun`
 --
 ALTER TABLE `tb_det_akun`
   ADD PRIMARY KEY (`id_det_akun`),
-  ADD UNIQUE KEY `id_akun` (`id_akun`);
+  ADD UNIQUE KEY `id_akun` (`id_akun`),
+  ADD KEY `id_akun_2` (`id_akun`);
 
 --
 -- Indexes for table `tb_det_produk`
 --
 ALTER TABLE `tb_det_produk`
   ADD PRIMARY KEY (`id_det_produk`),
-  ADD UNIQUE KEY `id_produk` (`id_produk`);
+  ADD UNIQUE KEY `id_produk` (`id_produk`),
+  ADD KEY `id_produk_2` (`id_produk`);
+
+--
+-- Indexes for table `tb_kategori`
+--
+ALTER TABLE `tb_kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `tb_keranjang`
+--
+ALTER TABLE `tb_keranjang`
+  ADD PRIMARY KEY (`id_keranjang`),
+  ADD KEY `id_produk` (`id_produk`),
+  ADD KEY `id_akun` (`id_akun`);
 
 --
 -- Indexes for table `tb_pembayaran`
 --
 ALTER TABLE `tb_pembayaran`
-  ADD PRIMARY KEY (`id_pembayaran`);
+  ADD PRIMARY KEY (`id_pembayaran`),
+  ADD KEY `id_transaksi` (`id_transaksi`);
+
+--
+-- Indexes for table `tb_produk`
+--
+ALTER TABLE `tb_produk`
+  ADD PRIMARY KEY (`id_produk`),
+  ADD KEY `id_kategori` (`id_kategori`),
+  ADD KEY `id_toko` (`id_toko`);
 
 --
 -- Indexes for table `tb_role_akun`
@@ -155,10 +251,18 @@ ALTER TABLE `tb_role_akun`
   ADD PRIMARY KEY (`id_role`);
 
 --
+-- Indexes for table `tb_toko`
+--
+ALTER TABLE `tb_toko`
+  ADD PRIMARY KEY (`id_toko`),
+  ADD KEY `id_akun` (`id_akun`);
+
+--
 -- Indexes for table `tb_transaksi`
 --
 ALTER TABLE `tb_transaksi`
-  ADD PRIMARY KEY (`id_transaksi`);
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_barang` (`id_keranjang`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -183,16 +287,94 @@ ALTER TABLE `tb_det_produk`
   MODIFY `id_det_produk` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `tb_kategori`
+--
+ALTER TABLE `tb_kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `tb_keranjang`
+--
+ALTER TABLE `tb_keranjang`
+  MODIFY `id_keranjang` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `tb_pembayaran`
 --
 ALTER TABLE `tb_pembayaran`
   MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `tb_produk`
+--
+ALTER TABLE `tb_produk`
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_toko`
+--
+ALTER TABLE `tb_toko`
+  MODIFY `id_toko` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `tb_transaksi`
 --
 ALTER TABLE `tb_transaksi`
   MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_akun`
+--
+ALTER TABLE `tb_akun`
+  ADD CONSTRAINT `tb_akun_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `tb_role_akun` (`id_role`);
+
+--
+-- Constraints for table `tb_det_akun`
+--
+ALTER TABLE `tb_det_akun`
+  ADD CONSTRAINT `tb_det_akun_ibfk_1` FOREIGN KEY (`id_akun`) REFERENCES `tb_akun` (`id_akun`);
+
+--
+-- Constraints for table `tb_det_produk`
+--
+ALTER TABLE `tb_det_produk`
+  ADD CONSTRAINT `tb_det_produk_ibfk_1` FOREIGN KEY (`id_produk`) REFERENCES `tb_produk` (`id_produk`);
+
+--
+-- Constraints for table `tb_keranjang`
+--
+ALTER TABLE `tb_keranjang`
+  ADD CONSTRAINT `tb_keranjang_ibfk_1` FOREIGN KEY (`id_produk`) REFERENCES `tb_produk` (`id_produk`),
+  ADD CONSTRAINT `tb_keranjang_ibfk_2` FOREIGN KEY (`id_akun`) REFERENCES `tb_akun` (`id_akun`);
+
+--
+-- Constraints for table `tb_pembayaran`
+--
+ALTER TABLE `tb_pembayaran`
+  ADD CONSTRAINT `tb_pembayaran_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `tb_transaksi` (`id_transaksi`);
+
+--
+-- Constraints for table `tb_produk`
+--
+ALTER TABLE `tb_produk`
+  ADD CONSTRAINT `tb_produk_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `tb_kategori` (`id_kategori`),
+  ADD CONSTRAINT `tb_produk_ibfk_2` FOREIGN KEY (`id_toko`) REFERENCES `tb_toko` (`id_toko`);
+
+--
+-- Constraints for table `tb_toko`
+--
+ALTER TABLE `tb_toko`
+  ADD CONSTRAINT `tb_toko_ibfk_1` FOREIGN KEY (`id_akun`) REFERENCES `tb_akun` (`id_akun`);
+
+--
+-- Constraints for table `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  ADD CONSTRAINT `tb_transaksi_ibfk_1` FOREIGN KEY (`id_keranjang`) REFERENCES `tb_keranjang` (`id_keranjang`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

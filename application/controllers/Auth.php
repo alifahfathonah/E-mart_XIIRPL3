03afe9d917d131e162d8255bde8f3ac59dadda0c
rifkyalfariz <rifkyalfariz2003@gmail.com>
2020-09-23 16:57:05 +0700

    Memperbarui function registrasi

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -20,7 +20,7 @@ class Auth extends CI_Controller{
 
 	public function registrasi()
 	{
-		$this->form_validation->set_rules('nama', 'Nama Depan', 'required|trim|min_length[3]|max_length[16]');
+		$this->form_validation->set_rules('nama', 'Nama', 'required|trim|min_length[3]|max_length[64]');
 		$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|max_length[64]|is_unique[tb_akun.email]');
 		$this->form_validation->set_rules('username', 'Username', 'required|trim|min_length[6]|max_length[16]|is_unique[tb_akun.username]');
 		$this->form_validation->set_rules('katasandi', 'Katasandi', 'required|trim|min_length[6]|matches[katasandi2]');
@@ -32,17 +32,26 @@ class Auth extends CI_Controller{
 			$this->load->view('main/registrasi');
 			$this->load->view('templates/auth_footer');
 		} else {
-			$data = [
+			$data1 = [
 				'nama' => htmlspecialchars($this->input->post('nama', true)),
-				'email' => htmlspecialchars($this->input->post('email', true)),
+				'email' => htmlspecialchars(strtolower($this->input->post('email', true))),
 				'username' => htmlspecialchars(strtolower($this->input->post('username', true))),
 				'katasandi' => password_hash($this->input->post('katasandi'), PASSWORD_DEFAULT),
 				'role_id' => 2,
 				'is_active' => 1,
 				'date_created' => time()
 			];
+			
+			$this->db->insert('tb_akun', $data1);
+			$last_id = $this->db->insert_id();
+			$data2 = [
+					'id_akun' => $last_id,
+					'img_profil' =>'default.jpg'
+			];
 
-			$this->db->insert('tb_akun', $data);
+			
+			
+			$this->db->insert('tb_det_akun',$data2);
 			redirect('auth');
 		}
 	}

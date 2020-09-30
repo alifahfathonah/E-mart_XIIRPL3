376be22c5fb8aab018e5316b529e5070b42aaad4
rifkyalfariz <rifkyalfariz2003@gmail.com>
2020-09-30 17:04:45 +0700

    Mengubah isi function index

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -11,9 +11,19 @@ class App extends CI_Controller{
         $query = 'SELECT * FROM tb_akun JOIN tb_det_akun ON tb_akun.id_akun = tb_det_akun.id_akun WHERE tb_akun.email = "'.$email.'"';
         $data['user'] = $this->db->query($query)->row_array();
 
-        $this->load->view('templates/header', $data);
-        $this->load->view('templates/u_topbar',$data);
-        $this->load->view('user/index', $data);
-        $this->load->view('templates/footer');
+        if(!$this->session->userdata('email')==true)
+        {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/u_bl_topbar',$data);
+            $this->load->view('user/index', $data);
+            $this->load->view('templates/footer');
+        }
+        else
+        {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/u_al_topbar',$data);
+            $this->load->view('user/index', $data);
+            $this->load->view('templates/footer');
+        }
     }
 }

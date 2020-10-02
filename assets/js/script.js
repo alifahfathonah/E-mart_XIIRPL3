0feff81d064a4c0df39ecdee79b626f97a586f27
rikiridwanzulkifli <kenshinrorouni084@gmail.com>
2020-10-02 14:50:55 +0700

    mnambahkan script js

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementByClassName("slideshow");
+  var dots = document.getElementByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slides 0; i = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+
+}

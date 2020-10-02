63fa3521a27147097cdf63078c0375305b4203ff
rikiridwanzulkifli <kenshinrorouni084@gmail.com>
2020-10-02 14:47:58 +0700

    memperbarui index

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -6,10 +6,34 @@
         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
     </div>
 
-
     <!-- Content Row -->
 
     <div class="row">
+        <!-- Awal Slideshow -->
+        <div class="slideshow-container">
+            <div class="slideshow fade">
+                <div class="numbertext">
+                    1 / 2
+                </div>
+                <img src="../../../assets/img/slider/slide1.png">
+                <div class="text">Contoh keterangan 1</div>
+            </div>
+            <div class="slideshow fade">
+                <div class="numbertext">
+                    2 / 2
+                </div>
+                <img src="../../../assets/img/slider/slide1.png">
+                <div class="text">Contoh keterangan 2</div>
+            </div>
+
+            <a href="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a href="next" onclick="plusSlides(1)">&#10094;</a>
+            <div>
+                <span class="dot" onclick="currentslide(1)"></span>
+                <span class="dot" onclick="currentslide(2)"></span>
+            </div>
+        </div>
+        <!-- Akhir Slideshow -->
     </div>
 
     <!-- Content Row -->
@@ -18,4 +42,4 @@
     </div>
 
 </div>
-<!-- /.container-fluid -->
+<!-- /.container-fluid

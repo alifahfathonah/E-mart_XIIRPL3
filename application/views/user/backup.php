7d43573b168ec6c183ad350f1560359604bee851
rikiridwanzulkifli <kenshinrorouni084@gmail.com>
2020-10-06 05:32:58 +0700

    membuat backup ndex di user

---
+++ application/views/user/backup.php
@@ -0,0 +1,27 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+    </div>
+
+    <!-- Content Row -->
+
+    <div class="row">
+
+    </div>
+
+    <!-- Content Row -->
+    <div class="row">
+        <div class="card" style="width: 150px; height: 250px;">
+            <center><img src="<?= base_url(); ?>assets/img/img-profil/default.png" width="140"></center>
+            <div class="text-barang mr-2 ml-2 mt-2">
+                <h5 class="mb-0" style="font-weight: bold;">barang</h5>
+                <p>asdasdasdasd ada a asda afafkjbh abafa</p>
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid

e22f719e3e1a84e5aa771d55c8eb8050ebd66b84
rikiridwanzulkifli <kenshinrorouni084@gmail.com>
2020-10-08 12:50:07 +0700

    memperbaiki image slider

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -1,31 +1,44 @@
+    <div class="top">
+
+    </div>
     <!-- Begin Page Content -->
     <div class="container-fluid" style="position: relative;">
 
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img class="d-block w-100" src="<?= base_url(); ?>assets/img/img-profil/default.png" alt="First slide">
+        <div class="row">
+            <div id="carouselExampleIndicators" style="width: 600px;" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="<?= base_url(); ?>assets/img/img-profil/default.png" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="<?= base_url(); ?>assets/img/img-profil/default.png" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="<?= base_url(); ?>assets/img/img-profil/default.png" alt="Third slide">
+                    </div>
                 </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="<?= base_url(); ?>assets/img/img-profil/default.png" alt="Second slide">
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="<?= base_url(); ?>assets/img/img-profil/default.png" alt="Third slide">
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" style="width: 20px;" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="slide-item">
+                        <img class="d-block w-100" src="<?= base_url(); ?>assets/img/img-profil/default.png">
+                    </div>
                 </div>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
         </div>
 
 
